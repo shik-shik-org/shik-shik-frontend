095266b2b5b1a33ab9a668d0b18d151e8bdec6fb
uasim1702 <uasim1702@abv.bg>
2025-12-07 11:14:56 +0200

    update

--- src/config/storeImages.ts
+++ src/config/storeImages.ts
@@ -89,7 +89,7 @@ export const getStoreImages = (city: string, address: string): string[] => {
   const cityLower = (city || "").toLowerCase().trim();
   const addressLower = (address || "").toLowerCase().trim();
 
-  if (cityLower.includes("montana") || cityLower.includes("монтана")) {
+  if (cityLower.includes("montana")) {
 
     if (addressLower.includes("65"))
       return storeImageMapping.Montana65 || [];
@@ -103,18 +103,18 @@ export const getStoreImages = (city: string, address: string): string[] => {
   if (cityLower.includes("razlog"))
     return storeImageMapping.razlog || [];
 
-  if (cityLower.includes("sofia") || cityLower.includes("софия")) {
+  if (cityLower.includes("sofia")) {
 
-    if (addressLower.includes("alabin") || addressLower.includes("алабин"))
+    if (addressLower.includes("alabin"))
       return storeImageMapping.Alabin || [];
 
-    if (addressLower.includes("4"))
+    if (addressLower.includes("veslec4"))
       return storeImageMapping.Veslec || [];
 
-    if (addressLower.includes("iskar") || addressLower.includes("искар"))
+    if (addressLower.includes("iskar"))
       return storeImageMapping.Iskar || [];
 
-    if (addressLower.includes("jenskipazar"))
+    if (addressLower.includes("stefan"))
       return storeImageMapping.JenskiPazar || [];
 
     if (addressLower.includes("boris"))
@@ -129,41 +129,35 @@ export const getStoreImages = (city: string, address: string): string[] => {
 
   const isPazardzhik =
     cityLower.includes("pazardjik") ||
-    cityLower.includes("pazardzhik") ||
-    cityLower.includes("пазарджик");
+    cityLower.includes("pazardzhik");
 
   if (isPazardzhik) {
     if (
-      addressLower.includes("екзарх") ||
-      addressLower.includes("йосиф") ||
       addressLower.includes("iosif") ||
       addressLower.includes("ekzarh")
     )
       return storeImageMapping.Ekzarh_iosif || [];
 
     if (
-      addressLower.includes("гладстон") ||
       addressLower.includes("gladston") ||
       addressLower.includes("gladstone")
     )
       return storeImageMapping.Gladstone_10 || [];
 
     if (
-      addressLower.includes("иван вазов") ||
       addressLower.includes("vazov")
     )
       return storeImageMapping.Ivan_Vazov_12 || [];
 
     if (
-      addressLower.includes("търговска") ||
       addressLower.includes("targovska")
     )
       return storeImageMapping.Targovska || [];
 
-    if (addressLower.includes("самуил") || addressLower.includes("samuil"))
+    if (addressLower.includes("samuil"))
       return storeImageMapping.Tsar_Samuil_4 || [];
 
-    if (addressLower.includes("шишман") || addressLower.includes("shishman"))
+    if (addressLower.includes("shishman"))
       return storeImageMapping.Tsar_Shishman_10 || [];
   }
 
